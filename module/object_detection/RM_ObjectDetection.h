3481f5eb8620f868b4de90eb876236d8622fb7ef
rcxxx <chuangxinr@gmail.com>
2021-05-11 19:12:08 +0800

    2021-05-11 update cv_yolo

---
+++ module/object_detection/RM_ObjectDetection.h
@@ -0,0 +1,34 @@
+#ifndef RM_OBJECTDETECTION_H
+#define RM_OBJECTDETECTION_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <opencv2/dnn/dnn.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+namespace ObjectDetection
+{
+// 相对路径，用的时候改成绝对路径
+const std::string yolov4_cfg = " ";
+const std::string yolov4_weights = " ";
+const std::string classes = " ";
+
+class CvYolov4
+{
+public:
+  CvYolov4();
+  ~CvYolov4();
+
+  void objDetection(cv::Mat &input_img, std::vector<cv::Rect> &obj_boxes);
+
+private:
+  cv::dnn::Net net;
+  std::vector<std::string> out_names;
+  std::vector<std::string> class_names;
+};
+
+} // namespace ObjectDetection
+
+#endif // RM_OBJECTDETECTION_H
